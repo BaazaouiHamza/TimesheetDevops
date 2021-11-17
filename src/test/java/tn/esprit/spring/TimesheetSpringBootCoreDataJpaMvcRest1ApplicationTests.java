c50da32bc1b12447d346b56a61ef0b96996db1e7
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2021-11-17 19:10:33 +0100

    first commit

---
+++ src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.java
@@ -0,0 +1,10 @@
+package tn.esprit.spring;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
