71ace6ef8779c70b4b4ce3a1518b73825bbc1c39
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2021-11-17 22:38:48 +0100

    test commit

--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.repository.DepartementRepository;
 
+@Service
 public class DepartementServiceImpl implements IDepartementService {
 
 	private static final Logger logger = LogManager.getLogger(DepartementServiceImpl.class);
